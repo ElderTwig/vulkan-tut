c8a061f0f03324f4b42127c24c9c79ce6f928092
ElderTwig <pqfrml@gmail.com>
2020-10-07 18:07:51 +0200

    Add validation layer

--- lib/helloTriangle.cpp
+++ lib/helloTriangle.cpp
@@ -1,5 +1,10 @@
 #include "helloTriangle.hpp"
+
+#include <gsl/gsl>
+
 #include <algorithm>
+#include <string>
+#include <vulkan/vulkan_core.h>
 
 auto
 HelloTriangle::initWindow() noexcept -> UniqueGLFWwindow
@@ -13,33 +18,101 @@ HelloTriangle::initWindow() noexcept -> UniqueGLFWwindow
 }
 
 auto
-extensionsSupported(char const** extensions, int nrExtentions) -> void
+getSupportedExtensions() -> std::vector<std::string>
 {
-    auto const supportedExtensions = vk::enumerateInstanceExtensionProperties();
+    auto const supportedExtensionProperties =
+            vk::enumerateInstanceExtensionProperties();
 
-    std::for_each(
-            extensions,
-            extensions + nrExtentions,
-            [&](auto const* extension) {
-                auto const found = std::cend(supportedExtensions)
-                                   != std::find(
-                                           std::cbegin(supportedExtensions),
-                                           std::cend(supportedExtensions),
-                                           extension);
-                if(!found) {
-                    std::cerr << "Extension " << extension
-                              << " not supported!\n";
-                }
+    auto supportedExtensions =
+            std::vector<std::string>(supportedExtensionProperties.size());
+
+    std::transform(
+            supportedExtensionProperties.cbegin(),
+            supportedExtensionProperties.cend(),
+            supportedExtensions.begin(),
+            [](auto&& property) -> std::string {
+                return property.extensionName;
             });
+
+    return supportedExtensions;
 }
 
 auto
-HelloTriangle::createInstace() -> vk::Instance
+checkExtensionSupport(std::vector<std::string> const& requiredExtensions)
+        -> void
+{
+    auto const supportedExtensions = getSupportedExtensions();
+    for(auto const& extension : requiredExtensions) {
+        auto const iterator = std::find(
+                supportedExtensions.cbegin(),
+                supportedExtensions.cend(),
+                extension);
+
+        if(iterator == supportedExtensions.cend()) {
+            throw std::runtime_error(extension + " not supported!");
+        }
+    }
+}
+
+auto
+getRequiredExtensions() -> std::vector<char const*>
 {
     auto extensionCount = uint32_t{};
 
     auto const** glfwExtentions =
             glfwGetRequiredInstanceExtensions(&extensionCount);
+
+    auto const span = gsl::make_span(glfwExtentions, extensionCount);
+
+    auto vec = std::vector<char const*>{std::cbegin(span), std::cend(span)};
+    vec.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    return vec;
+}
+
+auto
+getSupportedLayers() -> std::vector<std::string>
+{
+    auto const supportedLayerProps = vk::enumerateInstanceLayerProperties();
+    auto supportedLayers = std::vector<std::string>(supportedLayerProps.size());
+
+    std::transform(
+            std::cbegin(supportedLayerProps),
+            std::cend(supportedLayerProps),
+            std::begin(supportedLayers),
+            [](auto&& property) -> std::string { return property.layerName; });
+
+    return supportedLayers;
+}
+
+auto
+checkLayerSupport(std::vector<std::string> const& requiredLayers) -> void
+{
+    auto const supportedLayers = getSupportedLayers();
+
+    for(auto const& layer : requiredLayers) {
+        auto const iterator = std::find(
+                std::cbegin(supportedLayers),
+                std::cend(supportedLayers),
+                layer);
+
+        if(iterator == std::cend(supportedLayers)) {
+            throw std::runtime_error(layer + " layer is not supported!");
+        }
+    }
+}
+
+auto
+HelloTriangle::createInstace() -> vk::Instance
+{
+    auto const requiredExtensions = getRequiredExtensions();
+
+    checkExtensionSupport(std::vector<std::string>{
+            std::cbegin(requiredExtensions),
+            std::cend(requiredExtensions)});
+
+    auto constexpr layers = std::array{"VK_LAYER_KHRONOS_validation"};
+    checkLayerSupport(
+            std::vector<std::string>{std::cbegin(layers), std::cend(layers)});
 
     auto constexpr appInfo = vk::ApplicationInfo(
             "HelloTriangle",
@@ -51,10 +124,10 @@ HelloTriangle::createInstace() -> vk::Instance
     auto const instanceCreateInfo = vk::InstanceCreateInfo(
             {},
             &appInfo,
-            {},
-            {},
-            extensionCount,
-            glfwExtentions);
+            layers.size(),
+            layers.data(),
+            requiredExtensions.size(),
+            requiredExtensions.data());
 
     auto instance = vk::Instance{};
     auto const result =
@@ -63,8 +136,6 @@ HelloTriangle::createInstace() -> vk::Instance
     if(result != vk::Result::eSuccess) {
         throw std::runtime_error("Vk instance could not be created");
     }
-
-    extensionsSupported(glfwExtentions, extensionCount);
 
     return instance;
 }
